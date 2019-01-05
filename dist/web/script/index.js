06b19c998a2bc6da6c36f01012cddf5499f4fef9
Devin Don <I.INF@Outlook.com>
2019-01-05 21:13:08 +0800

    Update: 编译文件更新.

--- dist/web/script/index.js
+++ dist/web/script/index.js
@@ -141,6 +141,7 @@ class APP {
         this.api = new API('api/v1');
         this.dialog = new Dialog();
         this.starrysky = new StarrySky('background');
+        this.player = new Player(this.dialog);
         this.content = {
             home: {
                 name: '首页',
@@ -457,9 +458,12 @@ class APP {
           <td class="artist">${song.artist}</td>
           <td class="album">${song.album}</td>
           <td class="time">${song.time}</td>
-          <td class="control" onclick="Song.play(${song.id})">播放</td>
+          <td class="control">播放</td>
         </tr>
       `);
+            $tr.children('.control').click(e => {
+                this.player.toggle(song);
+            });
             $tbody.append($tr);
         }
         $table.append($thead, $tbody, $tfoot);
@@ -608,6 +612,67 @@ class Dialog {
         }
         else {
             return undefined;
+        }
+    }
+}
+/**
+ * 基于 QMPlayer 内核的网页播放器.
+ * 暂时没有考虑复用问题, 如果内联样式和资源的话, 看起来就很像 Angular 的 Component 了.
+ */
+class Player {
+    constructor(dialog) {
+        this.dialog = dialog;
+        this.player = new QMPlayer();
+        this.$player = $('<div class="card player init" data-mid="0"></div>');
+        const $info = $('<div class="info"></div>');
+        const $text = $('<div class="text"></div>');
+        this.$title = $(`<h2 class="title">歌曲</h2>`);
+        this.$artist = $(`<h3 class="artist">歌手</h3>`);
+        this.$albumname = $(`<p class="album">专辑</p>`);
+        this.$albumimage = $(`<img class="album" src="./assets/image/cd.png" alt="CD 封面">`);
+        const $controller = $('<div class="controller">');
+        this.$previous = $(`<button class="previous""></button>`);
+        this.$toggle = $(`<button class="toggle pause""></button>`);
+        this.$next = $(`<button class="next""></button>`);
+        $('#content-song').append(this.$player.append($info.append($text.append(this.$title, this.$artist, this.$albumname), this.$albumimage), $controller.append(this.$previous, this.$toggle, this.$next)));
+        this.init();
+    }
+    init() {
+        const albumAnime = anime({
+            targets: this.$albumimage[0],
+            rotate: 360,
+            duration: 12 * 1000,
+            easing: [0, 0, 0, 0],
+            autoplay: false,
+            loop: true
+        });
+        const toPlayStatus = (e) => {
+            this.$toggle.removeClass('pause').addClass('play');
+            albumAnime.play();
+        };
+        const toPauseStatus = (e) => {
+            this.$toggle.removeClass('play').addClass('pause');
+            albumAnime.pause();
+        };
+        this.player.on('play', toPlayStatus);
+        this.player.on('pause', toPauseStatus);
+        this.player.on('ended', toPauseStatus);
+        this.player.on('waiting', toPauseStatus);
+        this.player.on('error', e => {
+            toPauseStatus(e);
+            this.dialog.generate(`歌曲无法播放.`, 'error');
+        });
+    }
+    toggle(song) {
+        if (song) {
+            this.player.play(song.id);
+            this.$player.attr('data-mid', song.id);
+            this.$title.text(song.title);
+            this.$artist.text(song.artist);
+            this.$albumname.text(song.album);
+        }
+        else {
+            this.player.toggle(true);
         }
     }
 }
