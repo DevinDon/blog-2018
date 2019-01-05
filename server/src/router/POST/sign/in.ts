ca7c37c6cd4f3302b26f221c5d671bc728c499a2
Devin Don <I.INF@Outlook.com>
2019-01-05 14:04:01 +0800

    Addition: Post /sign/in.

---
+++ server/src/router/POST/sign/in.ts
@@ -0,0 +1,19 @@
+import { Middleware } from 'koa';
+
+export const signin: Middleware = async (c, next) => {
+  const req = c.body || {};
+  if (req.name === 'testname' && req.password === 'password') {
+    c.body = {
+      id: Date.now(),
+      status: true
+    };
+  } else {
+    c.body = {
+      id: Date.now(),
+      status: true
+    };
+  }
+  await next();
+};
+
+export default signin;
