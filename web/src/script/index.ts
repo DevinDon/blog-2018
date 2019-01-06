5af24175124339f5e534267c3f5a603d9a938e9e
DevinDon <DevinDon@Foxmail.com>
2019-01-06 12:33:37 +0800

    Optimization: 首页代码移动至 js 中.

--- web/src/script/index.ts
+++ web/src/script/index.ts
@@ -1,4 +1,34 @@
 /// <reference path="./API.ts" />
 /// <reference path="./APP.ts" />
 /// <reference path="./Dialog.ts" />
+/// <reference path="./Player.ts" />
 /// <reference path="./StarrySky.ts" />
+
+function toggleArticle() {
+  const $art = $('#article-home');
+  if ($art.hasClass('busy')) {
+    return false;
+  }
+  $art.addClass('busy');
+  if ($art.hasClass('show')) {
+    // 隐藏
+    anime({
+      targets: $art.get(0),
+      duration: 1500,
+      height: [475.81, 0],
+      opacity: [1, 0],
+      easing: 'easeInOutCirc',
+      complete: () => $art.removeClass('show').removeClass('busy')
+    });
+  } else {
+    // 展开
+    anime({
+      targets: $art.get(0),
+      duration: 1500,
+      height: [0, 475.81],
+      opacity: [0, 1],
+      easing: 'easeInOutCirc',
+      complete: () => $art.addClass('show').removeClass('busy')
+    });
+  }
+}
