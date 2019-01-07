ed4ea645374e4f4519c75356cb59876815921ad7
DevinDon <DevinDon@Foxmail.com>
2019-01-07 11:55:01 +0800

    Fixed: 修复统计数据中的客户端 IP 获取方式.

--- server/src/ware/statistic.ts
+++ server/src/ware/statistic.ts
@@ -4,7 +4,7 @@ import Statistic from '../entity/statistic.entity';
 export const statistic: Middleware = async (c, next) => {
   await next();
   Statistic.insert({
-    who: c.request.ip.slice(0, 128),
+    who: c.request.ips[0].slice(0, 128),
     when: Date.now(),
     where: c.request.path.slice(0, 255),
     what: c.method.slice(0, 32)
